cdaabec1d4ae2fa5f89f92654ef4220f64341248
João Domingos Pinheiro Neto <56659658+joaonetogit@users.noreply.github.com>
2023-06-24 14:40:41 -0300

    feat: theme light and dark in tailwind

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -4,7 +4,7 @@ const Widget = () => {
   return (
     <div className="w-[448px] rounded overflow-hidden">
       {/* Header */}
-      <div className="bg-zinc-800 py-4 px-6 flex items-center justify-between">
+      <div className=" bg-zinc-200 dark:bg-zinc-800 py-4 px-6 flex items-center justify-between">
         <span className="font-bold ">Notificação</span>
         <button className=" text-violet-500 font-bold text-xs hover:text-violet-400">
           MARCAR TODOS COMO VISTAS
@@ -13,14 +13,14 @@ const Widget = () => {
 
       {/* Recent Section */}
       <div>
-        <div className="bg-zinc-950 px-5 py-2 text-sm text-zinc-400">
+        <div className="bg-zinc-300 dark:bg-zinc-950 px-5 py-2 text-s text-zinc-500 dark:text-zinc-400">
           Recentes
         </div>
-        <div className="divide-y-2 divide-zinc-950">
-          <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+        <div className="divide-y-2 divide-zinc-300 dark:divide-zinc-950">
+          <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
             <Rocket className="w-6 h-6 text-violet-500 mt-3" />
             <div className="flex-1 flex flex-col gap-2">
-              <p className=" text-sm leading-relaxed text-zinc-100">
+              <p className=" text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">
                 Você recebeu um convite para fazer parte da empresa Rocketseat
               </p>
               <div className="text-xxs text-zinc-400 flex items-center gap-1">
@@ -29,10 +29,10 @@ const Widget = () => {
               </div>
             </div>
           </div>
-          <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+          <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
             <Rocket className="w-6 h-6 text-violet-500 mt-3" />
             <div className="flex-1 flex flex-col gap-2">
-              <p className=" text-sm leading-relaxed text-zinc-100">
+              <p className="text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">
                 Você recebeu um convite para fazer parte da empresa Rocketseat
               </p>
               <div className="text-xxs text-zinc-400 flex items-center gap-1">
@@ -41,7 +41,7 @@ const Widget = () => {
               </div>
             </div>
             <div className="flex gap-2 self-center">
-              <button className=" w-8 h-8 rounded flex items-center justify-center bg-zinc-800 hover:bg-zinc-700">
+              <button className=" w-8 h-8 rounded flex items-center justify-center bg-zinc-400 hover:bg-zinc-500 dark:bg-zinc-800 dark:hover:bg-zinc-700">
                 <X className="w-3 h-3 text-zinc-50" />
               </button>
               <button className=" w-8 h-8 rounded flex items-center justify-center bg-violet-500 hover:bg-violet-700">
@@ -54,14 +54,14 @@ const Widget = () => {
 
       {/* Old Section */}
       <div>
-        <div className="bg-zinc-950 px-5 py-2 text-sm text-zinc-400">
-          Antigas
+        <div className="bg-zinc-300 dark:bg-zinc-950 px-5 py-2 text-s text-zinc-500 dark:text-zinc-400">
+          Antigos
         </div>
-        <div className="divide-y-2 divide-zinc-950">
-          <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+        <div className="divide-y-2 divide-zinc-300 dark:divide-zinc-950">
+          <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
             <Rocket className="w-6 h-6 text-violet-500 mt-3" />
             <div className="flex-1 flex flex-col gap-2">
-              <p className=" text-sm leading-relaxed text-zinc-400">
+              <p className=" text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">
                 Você recebeu um convite para fazer parte da empresa Rocketseat
               </p>
               <div className="text-xxs text-zinc-400 flex items-center gap-1">
@@ -70,10 +70,10 @@ const Widget = () => {
               </div>
             </div>
           </div>
-          <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+          <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
             <Rocket className="w-6 h-6 text-violet-500 mt-3" />
             <div className="flex-1 flex flex-col gap-2">
-              <p className=" text-sm leading-relaxed text-zinc-400">
+              <p className="text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">
                 Você recebeu um convite para fazer parte da empresa Rocketseat
               </p>
               <div className="text-xxs text-zinc-400 flex items-center gap-1">
